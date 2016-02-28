ee9de90735b75879ce4e0510c5543354e00f2fbb
diry <yweba@ya.ru>
2016-02-29 03:11:45 +0600

    modify
    modify

--- core/index.php
+++ core/index.php
@@ -1,7 +1,13 @@
 <?php
 
+require "database.php";
+$database = new Database;
+require "billing.php";
+$billing = new Billing;
+
 class Core {
   function Core() {
     echo "ok\n";
   }
 }
+$core = new Core;
