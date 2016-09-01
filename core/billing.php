e6469c3458dae4e02e480b3fdc63bcbaceba37d0
diry <yweba@ya.ru>
2016-09-02 01:55:31 +0600

    update
    update

--- core/billing.php
+++ core/billing.php
@@ -1,11 +1,23 @@
 <?php
 
 class Billing {
-  function Billing($product_id = 0, $cost = 0) {
+  function Billing( $product_id = 0, $amount = 0 ) {
     if ( $product_id > 0 )
     {
-      if ( $cost > 0 ) return $product_id . "=" . $cost;
+      if ( $amount > 0 ) return $product_id . "=" . $amount;
     }
     return false;
+  }
+  function order( $userid = 0, $joindate = 0, $amount = 0 )
+  {
+    if ( $amount <= 0 ) return false;
+    if ( $joindate <= 0 ) $joindate = time();
+    $values = [
+      "userid" => $userid,
+      "joindate" => $joindate,
+      "amount" => $amount,
+    ]
+    $this->db->insert("payments", $values);
+    return $values;
   }
 }
